ffcfccf465d90488cb3aa2452ce83f5ca249eb46
lucaspolonuer <polonuer@gmail.com>
2016-09-12 02:19:03 -0300

    primer intento de scanner

---
+++ automata_scanner.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#define NUMESTADOS 15
+#define NUMCOLS 13
+#define TAMLEX 32+1
+
+typedef enum {
+ INICIO, FIN, LEER, ESCRIBIR, ID, CONSTANTE, PARENIZQUIERDO,
+ PARENDERECHO, PUNTOYCOMA, COMA, ASIGNACION, SUMA, RESTA, FDT
+} TOKEN;
+
+char buffer[TAMLEX];
+
+FILE * in;
+
+TOKEN Scanner(){
+	int tabla[NUMESTADOS][NUMCOLS] = {
+		{  1, 3 , 5 , 6 , 7 , 8 , 9 , 10, 11, 14, 13, 0, 14 },
+		{  1, 1 , 2 , 2 , 2 , 2 , 2 , 2 , 2 , 2 , 2 , 2 , 2 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99 , 99, 99, 99, 99 },
+		{  4,  3,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4, 4 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 14, 14, 14, 14, 14, 14, 14, 14, 14, 12, 14, 14, 14 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 },
+		{ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 } };
+
+	int car;
+	int col;
+	int estado = 0;
+	int i = 0;
+
+	do {
+		car = fgetc(in);
+		col = columna(car);
+		estado = tabla[estado][col];
+		if ( col != 11 ) { //si es espacio no lo agrega al buffer
+			buffer[i] = car;
+			i++;
+		}
+	}
+	while ( !estadoFinal(estado) && (estado != 14) );
+
+	buffer[i] = '\0'; //completé la cadena
+
+	switch ( estado )
+	{
+		case 2 : 
+			if ( col != 11 ){ //si el carácter espureo no es blanco…
+				ungetc(car, in); // lo retorna al flujo
+				buffer[i-1] = '\0';
+			}
+			return ID;
+		case 4 : 
+			if ( col != 11 ) {
+				ungetc(car, in);
+				buffer[i-1] = '\0';
+			}
+			return CONSTANTE;
+		case 5 : return SUMA;
+		case 6 : return RESTA;
+		case 7 : return PARENIZQUIERDO;
+		case 8 : return PARENDERECHO;
+		case 9 : return COMA;
+		case 10 : return PUNTOYCOMA;
+		case 12 : return ASIGNACION;
+		case 13 : return FDT;
+		case 14 : return ERRORLEXICO;
+	}
+
+	return 0;
+}
+
+int estadoFinal(int e){
+	if ( e == 0 || e == 1 || e == 3 || e == 11 || e == 14 ) return 0;
+	return 1;
+}
+
+int verifica(char * cad){
+	int i = 0;
+	while(cad[i] != '\0'){
+		if(cad[i] != 'a' && cad[i] != 'b') return 0;
+		i++;
+	}
+	return 1;
+}
+
+int columna(char c){
+	if ( isalpha(c) ) return 0;
+	if ( isdigit(c) ) return 1;
+	if ( c == '+' ) return 2;
+	if ( c == '-' ) return 3;
+	if ( c == '(' ) return 4;
+	if ( c == ')' ) return 5;
+	if ( c == ',' ) return 6;
+	if ( c == ';' ) return 7;
+	if ( c == ':' ) return 8;
+	if ( c == '=' ) return 9;
+	if ( c == EOF ) return 10;
+	if ( isspace(c) ) return 11;
+	return 12;
+}
+
+int automata(char * s){
+	static int tabla[6][2] = {{1,5}, {5,2}, {3,1}, {5,4}, {5,5}, {3,5}};
+	int estado = 0;
+	char car = *s;
+	while(car != '\0'){
+		estado = tabla[estado][columna(car)];
+		s++;
+		car = *s;
+	}
+	if(estado == 2 || estado == 4) return 1;
+	return 0;
+}
+
+int main(int argc, char * argv[]){
+	if(argc == 1){
+		printf("Debe ingresar una cadena\n");
+		return -1;
+	}
+	if(argc > 2){
+		printf("Debe ingresar solamente un argumento\n");
+		return -2;
+	}
+	/*
+	if(!verifica(argv[1])){
+		printf("La cadena tiene caracteres que no pertenecen al alfabeto\n");
+		return -3;
+	}*/
+	if(automata(argv[1]))
+		printf("La cadena es aceptada por ende pertenece al lenguaje\n");
+	else
+		printf("La cadena NO es aceptada por ende NO pertenece al lenguaje\n");
+	return 0;
+}
